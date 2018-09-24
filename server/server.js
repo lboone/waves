d6440ff1d27d1b76f7a6985b290cc5cb4d85db90
Lloyd Boone <lloyd@lambapps.com>
2018-09-24 16:02:20 -0400

    Add Brand and Wood

--- server/server.js
+++ server/server.js
@@ -15,10 +15,50 @@ app.use(cookieParser());
 
 //Models
 const { User } = require("./models/user");
+const { Brand } = require("./models/brand");
+const { Wood } = require("./models/wood");
 
 // Middlewares
 const { auth } = require("./middleware/auth");
+const { admin } = require("./middleware/admin");
 
+//=======================================
+//              BRAND
+//=======================================
+app.post("/api/v1/products/brand", auth, admin, (req, res) => {
+  const brand = new Brand(req.body);
+
+  brand.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true, brand: doc });
+  });
+});
+
+app.get("/api/v1/products/brands", (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(brands);
+  });
+});
+
+//=======================================
+//              WOODS
+//=======================================
+app.post("/api/v1/products/wood", auth, admin, (req, res) => {
+  const wood = new Wood(req.body);
+
+  wood.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true, wood: doc });
+  });
+});
+
+app.get("/api/v1/products/woods", (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(woods);
+  });
+});
 //=======================================
 //              USERS
 //=======================================
