e07c73ed70cfb09493f5919fc188f8f505309569
Lloyd Boone <lloyd@lambapps.com>
2018-09-24 19:21:56 -0400

    Add React Routes Layout Header Footer Structure

---
+++ client/src/routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Layout from "./hoc/Layout";
+
+import Home from "./components/Home";
+
+const Routes = () => {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Layout>
+  );
+};
+export default Routes;
