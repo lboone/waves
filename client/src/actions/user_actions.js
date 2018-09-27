a8731cb57cd3c6a8a751a821d7c173980944b56a
Lloyd Boone <lloyd@lambapps.com>
2018-09-27 18:11:45 -0400

    Add Header Links Login and Logout

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { USER_SERVER } from "../components/utils/misc";
-import { LOGIN_USER, REGISTER_USER, AUTH_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER, AUTH_USER, LOGOUT_USER } from "./types";
 
 export async function auth() {
   const resp = await axios.get(`${USER_SERVER}/auth`);
@@ -26,6 +26,15 @@ export async function registerUser(dataToSubmit) {
   const request = resp.data;
   return {
     type: REGISTER_USER,
+    payload: request
+  };
+}
+
+export async function logoutUser() {
+  const resp = await axios.get(`${USER_SERVER}/logout`);
+  const request = resp.data;
+  return {
+    type: LOGOUT_USER,
     payload: request
   };
 }
